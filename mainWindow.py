0b5dfd343e0bd105031192f30d462469680c31f1
noitu <dimalego35@gmail.com>
2022-11-27 13:35:06 +0300

    change styles

--- mainWindow.py
+++ mainWindow.py
@@ -1,4 +1,6 @@
 from PyQt5.QtWidgets import QWidget
+
+from const.CONSTANTS import SCRIPT_NAMES
 from const.page import Ui_Form
 
 
@@ -8,3 +10,11 @@ class MainWindow(QWidget, Ui_Form):
 
         self.setupUi(self)
 
+        self.pushButton_15.setText('ЗАПУСТИТЬ\nВСЕ')
+
+        buttons = [self.pushButton, self.pushButton_2, self.pushButton_3, self.pushButton_4, self.pushButton_5,
+                   self.pushButton_6, self.pushButton_7, self.pushButton_8, self.pushButton_9, self.pushButton_10,
+                   self.pushButton_11, self.pushButton_12, self.pushButton_13, self.pushButton_14]
+
+        for i, button in enumerate(buttons):
+            button.setText(SCRIPT_NAMES[i].upper())
