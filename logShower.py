1eff2d32cba93f86049efbef7897a9128075f931
noitu <dimalego35@gmail.com>
2022-12-02 19:50:04 +0300

    add log watching

---
+++ logShower.py
@@ -0,0 +1,11 @@
+from PyQt5.QtWidgets import QWidget
+
+from const.logWindow import Ui_Form
+
+
+class LogShower(QWidget, Ui_Form):
+    def __init__(self):
+        super(LogShower, self).__init__()
+
+        self.setupUi(self)
+
--- mainWindow.py
+++ mainWindow.py
@@ -1,18 +1,25 @@
+import os
+
+from PyQt5.QtCore import QProcess
 from PyQt5.QtCore import QProcess
 from PyQt5.QtGui import QColor, QPixmap
-from PyQt5.QtWidgets import QWidget, QGraphicsDropShadowEffect, QLabel
+from PyQt5.QtWidgets import QWidget, QGraphicsDropShadowEffect, QLabel, QMessageBox, QMenu, QPlainTextEdit
 
 from const.CONSTANTS import *
 from const.page import Ui_Form
+from logShower import LogShower
 
 
 class MainWindow(QWidget, Ui_Form):
     def __init__(self):
         super(MainWindow, self).__init__()
+
         self.common_icon = QPixmap('icons/common.png')
         self.done_icon = QPixmap('icons/done.png')
         self.loading_icon = QPixmap('icons/loading.png')
         self.error_icon = QPixmap('icons/broken.png')
+
+        self.log_window = None
 
         self.setupUi(self)
 
@@ -38,20 +45,19 @@ class MainWindow(QWidget, Ui_Form):
             shadow = QGraphicsDropShadowEffect()
             shadow.setParent(button)
             shadow.setEnabled(True)
-            shadow.setColor(QColor(256 // 5, 256 // 5, 256 // 5))
+            shadow.setColor(QColor(256 // 4, 256 // 4, 256 // 4))
             shadow.setXOffset(0)
             shadow.setBlurRadius(15)
 
             button.setGraphicsEffect(shadow)
-            # button.setIcon(self.common_icon)
-            circle = QPixmap('icons/circle.png')
-            # circle.scaled(119, 119)
 
             label = QLabel(button)
             label.setPixmap(self.common_icon)
             label.setStyleSheet('background: transparent;')
             label.move(24, 24)
+
             label.setParent(button)
+
             button.setProperty('state', 'common')
             button.setStyleSheet(COMMON_STYLE)
 
@@ -81,17 +87,15 @@ class MainWindow(QWidget, Ui_Form):
         self.start_script(button.text())
 
     def set_state(self, button, state):
-        # button.setProperty('state', state)
-        # button.style().unpolish(button)
-        # button.style().polish(button)
-        # button.update()
-        label = button.children()[-1]
+        label = button.children()[1]
         button.setToolTip(state + '...')
         if state == 'loading':
             label.setPixmap(self.loading_icon)
         elif state == 'done':
             label.setPixmap(self.done_icon)
         elif state == 'error':
+            # self.show_helper(button)
+
             label.setPixmap(self.error_icon)
 
     def start_script(self, name):
@@ -99,8 +103,6 @@ class MainWindow(QWidget, Ui_Form):
 
         path = f'\scripts\{prname}.py'
         path = self.script_path + path
-
-        logger.info(path)
 
         self.process = QProcess(self)
         process = self.process
@@ -110,7 +112,6 @@ class MainWindow(QWidget, Ui_Form):
         self.process.readyRead.connect(self.readout)
         self.process.start(path)
         logger.info('-------' + path + '--------------------------------')
-        logger.info(self.process.state())
 
     def readout(self):
         logger.info(self.process.readAll())
@@ -119,13 +120,28 @@ class MainWindow(QWidget, Ui_Form):
         state = process.exitCode()
         name = process.objectName()
         name = name.upper()
-        print(state, name)
+        logger.info((state, name))
+        widget = self.find_widget_by_name(name)
         if state == 0:
             logger.info(('DONE DONE DONE', name))
-            self.set_state(self.find_widget_by_name(name), 'done')
+            self.set_state(widget, 'done')
         else:
             self.error('UNKNOWN ERROR!! state is: ' + str(state))
-            self.set_state(self.find_widget_by_name(name), 'error')
+            self.menu = QMenu()
+            self.menu.addAction('Show logs', self.show_logs)
+            # self.menu.set
+            self.menu.addAction('Restart', lambda: self.start_current(widget))
+            self.menu.setAutoFillBackground(True)
+            self.menu.setStyleSheet(MENU_STYLE)
+            widget.setMenu(self.menu)
+            self.set_state(widget, 'error')
+
+    def show_logs(self):
+        self.log_window = LogShower()
+        with open('logs/logs.log', 'r') as log:
+            lines = log.readlines()[-100:]
+        self.log_window.plainTextEdit.setPlainText('\n'.join(lines))
+        self.log_window.show()
 
     def error(self, e):
         logger.error(('error:', e))
@@ -135,9 +151,10 @@ class MainWindow(QWidget, Ui_Form):
             if button.text() == name:
                 return button
 
-    def format_name(self, name):
-        if len(name) > 7:
-            return name.replace(' ', '\n')
+    def show_helper(self, widget):
+        box = QMessageBox()
+        box.addButton()
+        print(1)
 
     def startAnimation(self):
         pass
